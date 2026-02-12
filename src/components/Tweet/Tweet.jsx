312ebbb0f7832819e98571a7463c1108eed95aeb
Mark <mark.davidovich@gmail.com>
2026-02-12 13:47:32 +0200

    add basic scaffolding for Tweet and style

--- src/components/Tweet/Tweet.jsx
+++ src/components/Tweet/Tweet.jsx
@@ -1,6 +1,14 @@
 import style from "./Tweet.module.css";
 
 const Tweet = () => {
-  return <div>Tweet</div>;
+  return (
+    <div className={style.container}>
+      <div>
+        <p>Name</p>
+        <p>Date</p>
+      </div>
+      <p>tweet body</p>
+    </div>
+  );
 };
 export default Tweet;
