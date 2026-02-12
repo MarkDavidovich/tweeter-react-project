8472fa488bc43ea16b6c42c566d3a769204eb4e6
Mark <mark.davidovich@gmail.com>
2026-02-12 17:01:07 +0200

    add sort by descending tweets

--- src/components/TweetList/TweetList.jsx
+++ src/components/TweetList/TweetList.jsx
@@ -4,9 +4,14 @@ import style from "./TweetList.module.css";
 const TweetList = ({ tweets }) => {
   //recieves the tweets array
   //renders a list of tweets, automatically sorting them by newest
+
+  const sortedTweets = tweets.sort((a, b) => b.id - a.id);
+
+  console.log(sortedTweets);
+
   return (
     <div className={style.container}>
-      {tweets?.map((tweet) => (
+      {sortedTweets?.map((tweet) => (
         <Tweet key={tweet.id} user={tweet.user} datePosted={tweet.datePosted} text={tweet.text} />
       ))}
     </div>
